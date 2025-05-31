edb02843d48a0ac65dc8100adc41973ca3bbb463
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2025-06-01 01:48:45 +0200

    add settings to prifile

--- app/Http/Resources/Api/CustomerApiResource.php
+++ app/Http/Resources/Api/CustomerApiResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Api;
 
+use App\Models\Setting;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -24,7 +25,10 @@ class CustomerApiResource extends JsonResource
             'lang' => $this->lang,
             'wallet' => $this->wallet,
             'birth_date'=>$this->birth_date,
-            'gender'=>$this->gender
+            'gender'=>$this->gender,
+            'settings'=>[
+                'points_To_money' => '1000,'.ceil(1000 * Setting::getRate('point_to_money_rate')),
+            ]
         ];
 
     }
