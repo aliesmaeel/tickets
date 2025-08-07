8b76f824a5c838e1014985a938acd2aba8cdeb01
ali.esmaeel@visticle.de <ali.esmaeel@visticle.de>
2025-08-07 19:34:06 +0100

    add paymenttype to PaymentService.php

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -20,6 +20,7 @@ class PaymentService
                 'entityId'    => $entityId,
                 'amount'      => number_format($amount, 2, '.', ''),
                 'currency'    => $currency,
+                'paymentType' => 'DB',
                 'merchantTransactionId' => $merchantTransactionId,
             ]);
 
